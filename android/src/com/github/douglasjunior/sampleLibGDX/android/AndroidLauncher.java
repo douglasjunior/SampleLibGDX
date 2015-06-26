3de760592b447c766498a3f39c33d21c05621009
Douglas Nassif Roma Junior <nassifrroma@gmail.com>
2015-06-26 11:44:16 -0300

    first commit

---
+++ android/src/com/github/douglasjunior/sampleLibGDX/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.github.douglasjunior.sampleLibGDX.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.github.douglasjunior.sampleLibGDX.MainGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new MainGame(), config);
+	}
+}
